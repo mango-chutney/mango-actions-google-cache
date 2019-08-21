67a1ef328ab8a046ffd8c3d899029dbfe01222b4
Drew Hutton <drew.business2308@gmail.com>
2019-08-21 10:14:26 +0930

    Add checksum support for key

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import * as core from '@actions/core';
+import parseKey from './parseKey';
 import save_cache from './save_cache';
 import restore_cache from './restore_cache';
 
 // Todo: Test for gsutil installed
+// Todo: Work for multiple OS
 
 async function run() {
   try {
@@ -11,14 +13,25 @@ async function run() {
     const bucket = core.getInput('bucket', { required: true });
     const key = core.getInput('key', { required: true });
 
+    let parsedKey = parseKey(key);
+
     // Depending on type depends on if input is required
     if (type === 'save') {
       const files = core.getInput('files');
       const overwrite = core.getInput('overwrite');
-      await save_cache(bucket, key, files.split(' '), overwrite);
+
+      await save_cache(
+        bucket,
+        (await parsedKey) || key,
+        files.split(' '),
+        overwrite,
+      );
     } else if (type === 'restore') {
       const directory = core.getInput('directory');
-      await restore_cache(bucket, key, directory);
+      console.log('parsedKey:', await parsedKey);
+      console.log('key:', key);
+
+      await restore_cache(bucket, (await parsedKey) || key, directory);
     } else {
       throw new Error('Cache Types must be: save or restore');
     }
